e9eaa75f209696232947e99df346124849429a1d
monnag <gilingking@gmail.com>
2026-04-23 10:13:40 +0800

    Update version

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // HouseHill Frontend Configuration
 // Change API_BASE to your backend server URL in production
-const API_BASE = "https://househill.de.r.appspot.com";
+const API_BASE = "http://127.0.0.1:5000";
